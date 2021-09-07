b82bffc86601731658b434a3e650dff6396b35cd
torredo786 <53000828+torredo786@users.noreply.github.com>
2021-09-07 22:44:46 +0530

    Add files via upload

---
+++ diseasepredictor/urls.py
@@ -0,0 +1,34 @@
+"""diseasepredictor URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+#from MediBot.newlogin import login
+from django.contrib import admin
+from django.urls import path, re_path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'heart', views.heart, name="heart"),
+    re_path(r'diabetes', views.diabetes, name="diabetes"),
+    re_path(r'breast', views.breast, name="breast"),
+    re_path(r'bmi', views.bmi, name="bmi"),
+    path('', views.home, name="home"),
+    re_path('home$', views.home, name="home"),
+    re_path(r'login$', views.login, name="login"),
+    re_path(r'register$', views.register,name="register"),
+    re_path(r'forgot_password$', views.forgot_password,name="forgot_password"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
